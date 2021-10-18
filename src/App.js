d85778573d1ae41e95aaabdfa164b303a2ed81af
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-10-18 18:50:38 -0300

    feat: Página de Seja Bem Vindo finalizada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BoasVindas from './pages/BoasVindas/BoasVindas';
 
 function App() {
   return (
     <div>
- 
+        <BoasVindas />
     </div>
   );
 }
---
+++ src/Pages/BoasVindas/BoasVindas.js
@@ -0,0 +1,12 @@
+import './BoasVindas.css';
+const BoasVindas = () => {
+    return(
+        <div className="container-boas-vindas">
+            <img src="/img/logo.png" alt="Logo Blockchain Descomplicada" />
+            <h1>Seja Bem Vindo!</h1>
+            <p>Você pode começar a estudar agora mesmo, toque em começar.</p>
+            <button className="botao-padrao-1">começar</button>
+        </div>
+    )
+}
+export default BoasVindas;
