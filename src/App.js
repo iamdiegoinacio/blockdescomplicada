c4047bc55b6c724786c0de20443ee0c77ea9eec8
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-10-19 17:27:56 -0300

    feat: Ajustes na padronização das cores e ajustes das variáveis no CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './pages/Home/Home';
+import BoasVindas from './pages/BoasVindas/BoasVindas';
 
 function App() {
   return (
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return(
         <div className="container-home">
             <Topo />
-          
+            
         </div>
     )
 }
