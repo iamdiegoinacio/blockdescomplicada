9a1a79520c21b8d6a0cf44325b80ce647e5653d6
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 11:56:11 -0300

    fix: alterações component topo

--- src/components/Topo/Topo.js
+++ src/components/Topo/Topo.js
@@ -1,11 +1,13 @@
 import MenuHamburguer from "../MenuHamburguer/MenuHamburguer";
 import './Topo.css'
+import { Link } from 'react-router-dom';
+
 const Topo = () => {
     return (
         <div className="topo-container">
             <MenuHamburguer />
             <div className="foto-de-perfil">
-                <img src="/img/perfil.jpg" alt="Foto de perfil" />
+                <Link to="/perfil"><img src="/img/perfil.jpg" alt="Foto de perfil" /></Link>
             </div>
         </div>
     )
