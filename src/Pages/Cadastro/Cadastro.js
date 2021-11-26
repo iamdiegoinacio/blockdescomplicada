ba8ba4eeae91b49c57a1d54fb069391cf69dd6d9
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 11:59:20 -0300

    feat: backend e funcionalidades página cadastro

--- src/Pages/Cadastro/Cadastro.js
+++ src/Pages/Cadastro/Cadastro.js
@@ -1,27 +1,74 @@
+import { useState, useContext } from 'react';
 import './Cadastro.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+
 const Cadastro = () => {
+    const [ name, setName ] = useState('');
+    const [ surname, setSurname ] = useState('');
+    const [ email, setEmail ] = useState();
+    const [ password, setPassword] = useState();
+    const [ confirmPassword, setConfirmPassword ] = useState();
+
+    const { signUp } = useContext(AuthContext);
+
+    //Verifica se as senhas são iguais
+    function isPasswordTheSame(password, confirmPassword){
+        if(password === confirmPassword){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function handleRegister(e){
+        e.preventDefault();
+        if(name === ''){
+            alert('Nome não pode estar em branco.');
+        }else if(surname===''){
+            alert('Sobrenome não pode estar em branco');
+        }else if(email===''){
+            alert('Email não pode estar em branco');
+        }else{
+            const temp = isPasswordTheSame(password, confirmPassword);
+            if(temp){
+                signUp(email, password, name, surname);
+                setName('');
+                setSurname('');
+                setEmail('');
+                setPassword('');
+                setConfirmPassword('');
+            }else{
+                alert('As senhas não são iguais.');
+            }
+        }
+    }
+
     return (
         <div className="container-cadastro">
-            <img src="/img/tela-de-cadastro.jpg" alt="Imagem da tela de cadastro" />
+            {/* <img src="/img/tela-de-cadastro.jpg" alt="Imagem da tela de cadastro" /> */}
             <h1>Cadastro</h1>
-            <form>
-                <label>Nome e sobrenome</label>
-                <input placeholder="Digite o seu nome e sobrenome"/>
+            <form onSubmit={handleRegister}>
+                <label>Nome</label>
+                <input type="text" placeholder="Digite o seu nome" value={name} onChange={(e) => setName(e.target.value)}/>
 
-                <label>Login</label>
-                <input placeholder="Digite o seu e-mail" />
+                <label>Sobrenome</label>
+                <input type="text" placeholder="Digite o seu sobrenome" value={surname} onChange={(e) => setSurname(e.target.value)}/>
+
+                <label>E-mail</label>
+                <input type="email" placeholder="Digite o seu e-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
 
                 <label>Senha</label>
-                <input placeholder="Digite a sua senha" />
+                <input type="password" placeholder="Digite a sua senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
                 <label>Repita a sua senha</label>
-                <input placeholder="Repita a sua senha" />
+                <input type="password" placeholder="Repita a sua senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
 
                 <div className="botao-cadastro">
                     <button className="botao-padrao-1" type="submit">cadastrar</button>
                 </div>
             </form>
-            <a href="https://www.google.com.br">Já possui uma conta? Toque aqui e faça login.</a>
+            <Link to="/login">Já possui uma conta? Toque aqui e faça login.</Link>
         </div>
     )
 };
