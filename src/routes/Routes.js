2143725b26a2766f6c106c696630b2bb3e4fe3d2
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 16:48:45 -0300

    fix: alteração das rotas para adicionar o bloqueador de acesso via desktop

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -16,12 +16,13 @@ import Pergunta from '../pages/Pergunta/Pergunta';
 import Modulos from '../pages/Modulos/Modulos';
 import ModuloSelecionado from '../pages/ModuloSelecionado/ModuloSelecionado';
 import AlterarSenha from '../pages/AlterarSenha/AlterarSenha';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 
-
-export default function Routes(){
-    return(
+export default function Routes() {
+    return (
         <Switch>
-            <RouteWrapper exact path="/acessodesktop" component={AcessoDesktop} />
+            {/* <RouteWrapper path="/acessodesktop" component={AcessoDesktop} /> */}
             <RouteWrapper exact path="/boasvindas" component={BoasVindas} />
 
             <RouteWrapper exact path="/" component={Login} />
@@ -30,14 +31,14 @@ export default function Routes(){
             <RouteWrapper exact path="/recuperarsenha" component={RecuperarSenha} />
             <RouteWrapper exact path="/recuperarsenhasucesso" component={RecuperarSenhaSucesso} />
 
-            <RouteWrapper exact path="/home" component={Home} isPrivate/>
-            <RouteWrapper exact path="/perfil" component={Perfil} isPrivate/>
-            <RouteWrapper exact path="/progresso" component={Progresso} isPrivate/>
-            <RouteWrapper exact path="/questoes" component={Questoes} isPrivate/>
-            <RouteWrapper exact path="/pergunta" component={Pergunta} isPrivate/>
-            <RouteWrapper exact path="/modulos" component={Modulos} isPrivate/>
-            <RouteWrapper exact path="/modulos/:moduloselecionado" component={ModuloSelecionado} isPrivate/>
-            <RouteWrapper exact path="/alterarsenha" component={AlterarSenha} isPrivate/>
+            <RouteWrapper exact path="/home" component={Home} isPrivate />
+            <RouteWrapper exact path="/perfil" component={Perfil} isPrivate />
+            <RouteWrapper exact path="/progresso" component={Progresso} isPrivate />
+            <RouteWrapper exact path="/questoes" component={Questoes} isPrivate />
+            <RouteWrapper exact path="/pergunta" component={Pergunta} isPrivate />
+            <RouteWrapper exact path="/modulos" component={Modulos} isPrivate />
+            <RouteWrapper exact path="/modulos/:moduloselecionado" component={ModuloSelecionado} isPrivate />
+            <RouteWrapper exact path="/alterarsenha" component={AlterarSenha} isPrivate />
         </Switch>
     )
 }
