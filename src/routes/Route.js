890ac31a08a9a7aa44523a245c9e48310059711e
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 16:47:39 -0300

    feat: bloqueador de acesso via desktop nas rotas

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -6,20 +6,18 @@ import { AuthContext } from '../contexts/AuthContext';
     RouteWraper recebe um componente, uma variávei isPrivate indicando se é uma rota privada ou não, e o ...rest que são os restantes
     atributos do componente Route 
 */
-export default function RouteWrapper({component : Component, isPrivate, ...rest}){
-    const { currentUser, signed} = useContext(AuthContext);
-
-    console.log("CURRENT USER => ",currentUser);
-
-    if(!signed && isPrivate){
+export default function RouteWrapper({ component: Component, isPrivate, ...rest }) {
+    const { currentUser, signed } = useContext(AuthContext);
+    
+    if (!signed && isPrivate && window.screen.width <= 768) {
         return <Redirect to="/login" />
     }
 
-    if(signed && !isPrivate){
+    if (signed && !isPrivate && window.screen.width <= 768) {
         return <Redirect to="/home" />
     }
-
-    return(
-        <Route {...rest} render={props => (<Component {...props}/>)} />
+    
+    return (
+        <Route {...rest} render={props => (<Component {...props} />)} />
     )
 }
