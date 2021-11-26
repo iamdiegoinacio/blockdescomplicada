9d9462947cff43f09b6886d601dd0ce76ce51c37
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 16:45:53 -0300

    feat: bloqueador de acesso via desktop

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes/Routes';
 import AuthProvider from './contexts/AuthContext';
+import AcessoDesktop from './pages/AcessoDesktop/AcessoDesktop';
 
 function App() {
   return (
     <div>
         <AuthProvider>
           <BrowserRouter>
-            <Routes />
+            {window.screen.width <= 768 ? 
+              <Routes />
+              :
+              <AcessoDesktop />
+            }
           </BrowserRouter>
         </AuthProvider>
     </div>
