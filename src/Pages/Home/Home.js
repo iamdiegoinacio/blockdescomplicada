72670e55c6f0b8adac31046adf5a9558e62b97a9
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 11:53:59 -0300

    fix: alterações página Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,8 @@ import './Home.css';
 import Topo from '../../components/Topo/Topo';
 import { FaUserAlt, FaRegQuestionCircle, FaGg, FaCodepen } from "react-icons/fa";
 import BotaoHome from '../../components/BotaoHome/BotaoHome';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
     return (
         <div className="container-home">
@@ -9,10 +11,10 @@ const Home = () => {
             <div className="home-conteudo">
                 <img src="/img/tela-inicial.jpg" alt="Imagem da página home" />
                 <h1>Home</h1>
-                <BotaoHome Setup={{Icone: <FaUserAlt />, texto: "Perfil", tipo: 1}} />
-                <BotaoHome Setup={{Icone: <FaRegQuestionCircle />, texto: "Questões", tipo: 2}} />
-                <BotaoHome Setup={{Icone: <FaGg />, texto: "Módulos", tipo: 1}} />
-                <BotaoHome Setup={{Icone: <FaCodepen/>, texto: "Progresso", tipo: 2}} />
+                <Link to="/perfil"><BotaoHome Setup={{Icone: <FaUserAlt />, texto: "Perfil", tipo: 1}} /></Link>
+                <Link to="/questoes"><BotaoHome Setup={{Icone: <FaRegQuestionCircle />, texto: "Questões", tipo: 2}} /></Link>
+                <Link to="/modulos"><BotaoHome Setup={{Icone: <FaGg />, texto: "Módulos", tipo: 1}} /></Link>
+                <Link to="/progresso"><BotaoHome Setup={{Icone: <FaCodepen/>, texto: "Progresso", tipo: 2}} /></Link>
             </div>
         </div>
     )
