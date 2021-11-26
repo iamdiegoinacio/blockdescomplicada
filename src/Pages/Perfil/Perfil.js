4b8ecf42819fd3e304a48d4dd3e0793cc156cd94
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 11:50:12 -0300

    feat: fetch de dados na página perfil

--- src/Pages/Perfil/Perfil.js
+++ src/Pages/Perfil/Perfil.js
@@ -1,20 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Perfil.css';
 import Topo from '../../components/Topo/Topo';
+
 const Perfil = () => {
+    const [user, setUser] = useState('');
+    useEffect(() => {
+        async function getDataUser() {
+            const currentUser = localStorage.getItem('CurrentUser');
+
+            setUser(JSON.parse(currentUser));
+        }
+
+        getDataUser();
+    }, [])
+
     return (
         <div className="container-perfil">
             <Topo />
             <div className="perfil-conteudo">
                 <img src="/img/tela-perfil.jpg" alt="Imagem da página Perfil" />
                 <h1>Perfil</h1>
-                <form>
-                    <label>Nome e sobrenome</label>
-                    <input placeholder="" value="Diego Inácio" />
+                {user &&
+                    <form>
+                        <label>Nome</label>
+                        <input value={user.name} />
 
-                    <label>Login</label>
-                    <input placeholder="Digite o seu e-mail" value="diegoinacio@gmail.com" />
-                </form>
-                <a href="https://www.google.com.br">Deseja alterar a sua senha? Clique aqui.</a>
+                        <label>Sobrenome</label>
+                        <input value={user.surname} />
+
+                        <label>E-mail</label>
+                        <input value={user.email} />
+                    </form>
+                }
+                <Link to="/alterarsenha" >Deseja alterar a sua senha? Toque aqui.</Link>
             </div>
         </div>
     )
