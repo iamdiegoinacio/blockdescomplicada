c097a7349ee2b3c50e6cbe7be23adccd3156308b
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 11:55:18 -0300

    feat: backend página Progresso

--- src/Pages/Progresso/Progresso.js
+++ src/Pages/Progresso/Progresso.js
@@ -1,6 +1,21 @@
 import './Progresso.css';
 import Topo from '../../components/Topo/Topo';
+import { useState, useEffect} from 'react';
+
 const Progresso = () => {
+    const [user, setUser] = useState({});
+    
+    useEffect(() => {
+        async function getDataUser(){
+            const currentUser = await localStorage.getItem('CurrentUser');
+
+            setUser(JSON.parse(currentUser));
+            console.log(user);
+        }
+
+        getDataUser();
+    }, [])
+    
     return (
         <div className="container-progresso">
             <Topo />
@@ -9,7 +24,7 @@ const Progresso = () => {
                 <h1>Progresso</h1>
                 <h2>você concluiu</h2>
                 <div className="porcentagem-progresso">
-                    <h3>25%</h3>
+                    <h3>{user.progress}%</h3>
                     <h4>dos módulos</h4>
                 </div>
             </div>
