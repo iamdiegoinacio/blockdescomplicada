a15b3daf3dbdb9334a2421e2c7b129cc3edb771b
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-11-26 11:54:44 -0300

    feat: login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,29 @@
+import { useState, useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { Link } from 'react-router-dom';
+
 import './Login.css';
-const Login =  () => {
-    return(
+const Login = () => {
+    const [ email, setEmail ] = useState();
+    const [ password, setPassword ] = useState();
+
+    const { signIn } = useContext(AuthContext);
+
+    function handleLogin(e){
+        e.preventDefault();
+        signIn(email, password);
+    }
+
+    return (
         <div className="container-login">
             <img src="/img/tela-de-login.jpg" alt="Imagem da tela de login" />
             <h1>Login</h1>
-            <form>
-                <label>Login</label>
-                <input placeholder="Digite o seu e-mail"/>
+            <form onSubmit={handleLogin}>
+                <label>E-mail</label>
+                <input type="email" placeholder="Digite o seu e-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
 
                 <label>Senha</label>
-                <input placeholder="Digite a sua senha"/>
+                <input type="password" placeholder="Digite a sua senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
                 <div className="botao-login">
                     <button className="botao-padrao-1" type="submit">
@@ -17,12 +31,13 @@ const Login =  () => {
                     </button>
                 </div>
             </form>
-            <a href="https://www.google.com.br">
+            <Link to="/cadastro">
                 Não possui uma conta? Clique aqui e cadastre-se
-            </a>
-            <a href="https://www.google.com.br">
+            </Link>
+            <Link to="/recuperarsenha">
                 Esqueceu a sua senha? Clique aqui.
-            </a>
+            </Link>
+
         </div>
     );
 }
