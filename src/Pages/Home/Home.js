94bdf7e1375b90edfa0b6dca3ea44ea5a4d723c5
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-12-07 17:29:23 -0300

    feat: refatoração código Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,20 +1,20 @@
 import './Home.css';
 import Topo from '../../components/Topo/Topo';
 import { FaUserAlt, FaRegQuestionCircle, FaGg, FaCodepen } from "react-icons/fa";
-import BotaoHome from '../../components/BotaoHome/BotaoHome';
+import HomeButton from '../../components/HomeButton/HomeButton';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className="container-home">
+        <div className="home-container">
             <Topo />
-            <div className="home-conteudo">
+            <div className="home-content">
                 <img src="/img/tela-inicial.jpg" alt="Imagem da página home" />
                 <h1>Home</h1>
-                <Link to="/perfil"><BotaoHome Setup={{Icone: <FaUserAlt />, texto: "Perfil", tipo: 1}} /></Link>
-                <Link to="/questoes"><BotaoHome Setup={{Icone: <FaRegQuestionCircle />, texto: "Questões", tipo: 2}} /></Link>
-                <Link to="/modulos"><BotaoHome Setup={{Icone: <FaGg />, texto: "Módulos", tipo: 1}} /></Link>
-                <Link to="/progresso"><BotaoHome Setup={{Icone: <FaCodepen/>, texto: "Progresso", tipo: 2}} /></Link>
+                <Link to="/perfil"><HomeButton Setup={{Icon: <FaUserAlt />, text: "Perfil", type: 1}} /></Link>
+                <Link to="/questoes"><HomeButton Setup={{Icon: <FaRegQuestionCircle />, text: "Questões", type: 2}} /></Link>
+                <Link to="/modulos"><HomeButton Setup={{Icon: <FaGg />, text: "Módulos", type: 1}} /></Link>
+                <Link to="/progresso"><HomeButton Setup={{Icon: <FaCodepen/>, text: "Progresso", type: 2}} /></Link>
             </div>
         </div>
     )
