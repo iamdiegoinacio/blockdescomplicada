ed40a40e16930c11d6ed0effd9cb9454b9629dff
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-12-07 17:43:07 -0300

    feat: refatoração página DesktopAccess

---
+++ src/Pages/DesktopAccess/DesktopAccess.js
@@ -0,0 +1,11 @@
+import './DesktopAccess.css';
+const DesktopAccess = () => {
+    return (
+        <div className="desktop-access-container">
+            <img src="/img/tela-de-aviso-acesso-desktop.jpg" alt="Imagem da tela de cadastro" />
+            <h1>Acessando de um desktop?</h1>
+            <span>Desculpe, esse site só pode ser acessado via smartphone</span>
+        </div>
+    )
+}
+export default DesktopAccess;
