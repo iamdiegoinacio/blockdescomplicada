ceca2e26d28cb37a17d041025ee7f0b6a0ec65a1
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-12-08 00:13:02 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -12,16 +12,6 @@ const firebaseConfig = {
   appId: process.env.API_ID
 };
 
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCFg7wAncE3EickpZlWG8dgD90x1rJK6Ic",
-//   authDomain: "blockchaindescomplicada.firebaseapp.com",
-//   projectId: "blockchaindescomplicada",
-//   storageBucket: "blockchaindescomplicada.appspot.com",
-//   messagingSenderId: "31204662979",
-//   appId: "1:31204662979:web:f14ff438d2eeb2737d8c76"
-// };
-
 if(!firebase.apps.length){
   const app = firebase.initializeApp(firebaseConfig);
 }
